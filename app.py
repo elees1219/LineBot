7cdc6f90143ca836677960e8bcd62679478b0a42
RaenonX JELLYCAT <maplestory0710@gmail.com>
2017-03-20 17:51:52 +0800

    UPD

--- app.py
+++ app.py
@@ -77,4 +77,4 @@ if __name__ == "__main__":
     arg_parser.add_argument('-d', '--debug', default=False, help='debug')
     options = arg_parser.parse_args()
 
-    app.run(debug=options.debug, port=options.port)
+    app.run(debug=options.debug, port=options.port, host='0.0.0.0')
