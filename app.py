76da487059b7f37f1db16cbbd31af5340863e77c
RaenonX JELLYCAT <maplestory0710@gmail.com>
2017-03-21 20:12:35 +0800

    X

--- app.py
+++ app.py
@@ -7,10 +7,7 @@ import tempfile
 
 # Database import
 import psycopg2
-try:
-    from urllib.parse import urlparse
-except ImportError:
-    from urlparse import urlparse
+import urlparse
 
 from flask import Flask, request, abort
 
