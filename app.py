bc0dc4e6fa88f53653f86e6905b95a93676f3151
RaenonX JELLYCAT <maplestory0710@gmail.com>
2017-03-25 10:24:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -145,7 +145,7 @@ def handle_text_message(event):
                     paramQ = split(param1, '  ', 2)
                     param1, param2 = [paramQ.pop(0) if len(paramQ) > 0 else None for i in range(2)]
                     if int(param2) - int(param1) <= 15:
-                        results = db.search_keyword(int(param1), int(param2))
+                        results = db.search_keyword_index(param1, param2)
                     else:
                         results = None
                         text = 'Maximum selecting range by ID is 15.'
