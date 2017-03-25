e0c73ff43ed5f61c2b38d29210c48abf5c7dec9c
RaenonX JELLYCAT <maplestory0710@gmail.com>
2017-03-25 15:32:11 +0800

    XX

--- app.py
+++ app.py
@@ -115,8 +115,8 @@ def handle_text_message(event):
                         text = u'Pair Added. Total: {len}\n'.format(len=len(results))
                         for result in results:
                             text += u'ID: {id}\n'.format(id=result[kwdict_col.id])
-                            text += u'Keyword: {kw}\n'.format(kw=result[kwdict_col.keyword])
-                            text += u'Reply: {rep}\n'.format(rep=result[kwdict_col.reply])
+                            text += u'Keyword: {kw}\n'.format(kw=str(result[kwdict_col.keyword]))
+                            text += u'Reply: {rep}\n'.format(rep=str(result[kwdict_col.reply]))
 
                     api.reply_message(rep, TextSendMessage(text=text))
                 # DELETE keyword
