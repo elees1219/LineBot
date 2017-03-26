3c9b37cecc4de179d477c3170aa8e688e4e61ebf
RaenonX JELLYCAT <maplestory0710@gmail.com>
2017-03-26 16:38:13 +0800

    Fix check 'cmd' bug

--- app.py
+++ app.py
@@ -111,7 +111,7 @@ def handle_text_message(event):
                 try:
                     params = split(oth, splitter, split_count[oth.split(splitter)[0]] - 1)
                     cmd, param1, param2, param3 = [params.pop(0) if len(params) > 0 else None for i in range(4)]
-                    if cmd not in split_count.keys:
+                    if cmd not in split_count:
                         text = u'Invalid Command: {cmd}. Please recheck the user manual.'.format(cmd=ex.message)
                         api.reply_message(rep, TextSendMessage(text=text))
                         return
