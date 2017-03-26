f3d795a2cf83f7d542bac783cf7f62ce380c1487
RaenonX JELLYCAT <maplestory0710@gmail.com>
2017-03-26 21:21:46 +0800

    Debug GA SS

--- app.py
+++ app.py
@@ -368,6 +368,10 @@ def handle_text_message(event):
                         perm = 0
                         pert = 'Permission: User'
 
+                    print param_count
+                    print param1, param2, param3, param4, param5, param6
+                    print perm
+
                     if isinstance(event.source, SourceUser):
                         uid = event.source.user_id
                         if perm >= 1 and param_count == 4:
