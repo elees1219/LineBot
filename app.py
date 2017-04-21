a3499b09c03113eeb1fe0a1aeec20f4477ef7eb5
RaenonX JELLYCAT <maplestory0710@gmail.com>
2017-04-22 02:26:52 +0800

    Pending NewFunction - Auto Reply Block
    Fix Sticker Reply Bug

--- app.py
+++ app.py
@@ -543,6 +543,8 @@ def handle_text_message(event):
                 result = res[0]
                 if result[kwdict_col.is_sticker]:
                     reply = sticker_png_url(result[kwdict_col.reply].decode('utf8'))
+                    print reply
+                    print result
                     api.reply_message(rep, ImageMessage(originalContentUrl=reply, previewImageUrl=reply))
                 else:
                     reply = result[kwdict_col.reply].decode('utf8')
@@ -550,7 +552,7 @@ def handle_text_message(event):
     except exceptions.LineBotApiError as ex:
         text = u'Line Bot Api Error. Status code: {sc}\n\n'.format(sc=ex.status_code)
         for err in ex.error.details:
-            text += u'Property: {prop}\nMessage: {msg}'.format(prop=err.property, msg=err.message)
+            text += u'Property: {prop}\nMessage: {msg}\n'.format(prop=err.property, msg=err.message)
         api.reply_message(rep, TextSendMessage(text=text))
     except Exception as exc:
         exc_type, exc_obj, exc_tb = sys.exc_info()
