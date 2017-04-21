5905693336b445bdb076a718ca1679a78be3b58a
RaenonX JELLYCAT <maplestory0710@gmail.com>
2017-04-22 00:23:13 +0800

    StickerMessage Windows Picture Path Bug

--- app.py
+++ app.py
@@ -616,7 +616,7 @@ def handle_sticker_message(event):
              TextSendMessage(text='Picture Location on Android(png):\nemulated\\0\\Android\\data\\jp.naver.line.android\\stickers\\{pck_id}\\{stk_id}'.format(
                 pck_id=package_id, 
                 stk_id=sticker_id)),
-             TextSendMessage(text='Picture Location on Windows PC(png):\nC:\\Users\\RaenonX\\AppData\\Local\\LINE\\Data\\Sticker\\{pck_id}\\{stk_id}'.format(
+             TextSendMessage(text='Picture Location on Windows PC(png):\nC:\\Users\\{USER_NAME}\\AppData\\Local\\LINE\\Data\\Sticker\\{pck_id}\\{stk_id}'.format(
                 pck_id=package_id, 
                 stk_id=sticker_id))]
         )
