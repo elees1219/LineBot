037c1ebf28cc20fa6fe23e0983224342f3c54e92
RaenonX JELLYCAT <maplestory0710@gmail.com>
2017-04-23 00:44:49 +0800

    Debug

--- app.py
+++ app.py
@@ -858,11 +858,9 @@ def reply_message_by_keyword(channel_id, token, keyword, is_sticker_kw):
 
 def rec_error(details):
     if details is not None:
-        rec['error'] = 'Error Recorded at {time}'.format(time=datetime.now() + timedelta(hours=8))
-        rec['error'] += '\n\n'
-        rec['error'] += details  
-
-    print rec
+        rec['Error'] = 'Error Recorded at {time}'.format(time=datetime.now() + timedelta(hours=8))
+        rec['Error'] += '\n\n'
+        rec['Error'] += details  
 
 
 if __name__ == "__main__":
