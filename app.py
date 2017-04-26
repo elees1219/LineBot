c7b43069fbc69f9cb2c16b8c8a0c4e8385ba92fc
RaenonX JELLYCAT <maplestory0710@gmail.com>
2017-04-26 19:02:35 +0800

    Emend error message in 'A'

--- app.py
+++ app.py
@@ -340,6 +340,8 @@ def handle_text_message(event):
                                                                  top='(top)' if is_top[cmd] else '')
                             for result in results:
                                 text += kw_dict_mgr.entry_basic_info(result)
+                        else:
+                            text = 'Pair adding failed. Ensure none of parameter is empty.'
 
                     api_reply(rep, TextSendMessage(text=text))
                 # DELETE keyword & DELETE top keyword
