a18742e6c909c6f632efcd1134a8204cf36da7d9
RaenonX JELLYCAT <maplestory0710@gmail.com>
2017-04-29 00:58:47 +0800

    Modify 'K' 'P' output

--- app.py
+++ app.py
@@ -464,11 +464,11 @@ def handle_text_message(event):
                         text += u'Message Replied: {repl}\n\n'.format(repl=rec['Msg_Replied'])
                         text += u'System command called count (including failed): {t}\n{info}'.format(t=rec['JC_called'], info=cmd_called_time)
                         
-                        text2 = u'Data Collected Full Time:\n\n'
-                        text2 += u'Count of Keyword Pair: {ct} (STK KW: {stk_kw}, PIC REP: {pic_rep})\n'.format(ct=kwpct,
+                        text2 = u'Data Collected Full Time\n\n'
+                        text2 += u'Count of Keyword Pair: {ct}\n(STK KW: {stk_kw}, PIC REP: {pic_rep})\n'.format(ct=kwpct,
                                                                                                                 stk_kw=kwd.sticker_keyword_count(),
                                                                                                                 pic_rep=kwd.picture_reply_count())
-                        text2 += u'Count of Active Keyword Pair: {ct} (STK KW: {stk_kw}, PIC REP: {pic_rep})\n'.format(ct=kwd.row_count(True),
+                        text2 += u'Count of Active Keyword Pair: {ct}\n(STK KW: {stk_kw}, PIC REP: {pic_rep})\n'.format(ct=kwd.row_count(True),
                                                                                                                        stk_kw=kwd.sticker_keyword_count(True),
                                                                                                                        pic_rep=kwd.picture_reply_count(True))
                         text2 += u'Count of Reply: {crep}\n\n'.format(crep=kwd.used_count_sum())
