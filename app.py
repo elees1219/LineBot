0deae4cb1dae7f08f97c2a9d07e1c8b6bc56ae2c
RaenonX JELLYCAT <maplestory0710@gmail.com>
2017-04-29 17:06:50 +0800

    Debug

--- app.py
+++ app.py
@@ -222,10 +222,6 @@ def handle_text_message(event):
                            'C': 2, 'I': 3, 'K': 4, 'P': 2, 'G': 2, 'GA': 3, 
                            'H': 2, 'SHA': 3, 'O': 3, 'B': 3, 'U': 3}
 
-            print head
-            print cmd
-            print oth
-
             is_top = {'S': True, 'A': False, 'M': True, 'D': False, 'R': True, 'Q': False, 
                       'C': True, 'I': False, 'K': False, 'P': False, 'G': False, 'GA': True, 
                       'H': False, 'SHA': False, 'O': False, 'B': False, 'U': False}
