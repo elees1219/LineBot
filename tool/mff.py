e12e9ea20c21cf38de8cf97b9deec785d205a475
RaenonX JELLYCAT <maplestory0710@gmail.com>
2017-05-14 07:09:36 +0800

    Debug

--- tool/mff.py
+++ tool/mff.py
@@ -118,7 +118,7 @@ class job(object):
     return self._data
 
   def __repr__(self):
-      return '\n'.join('{} - {}'.format(value.description.decode('utf-8'), value.value) for key, value in self._data.items())
+      return u'\n'.join(u'{} - {}'.format(value.description, value.value) for key, value in self._data.items())
     
 class dmg_bonus(object):
   """NOTICE: enter value without percentage."""
@@ -158,7 +158,7 @@ class dmg_bonus(object):
     self._value = float(value)
     
   def __repr__(self):
-    return u'Description: {}, Key: {}, Data Set: {}, Value: (B){} / (NB){}'.format(self._description.decode('utf-8'), 
+    return u'Description: {}, Key: {}, Data Set: {}, Value: (B){} / (NB){}'.format(self._description, 
                                                                                    self._key, 
                                                                                    self._value_set, 
                                                                                    self.val(), 
