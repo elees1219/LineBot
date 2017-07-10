cdfccfdabfd84ca381ace47476a5c30aa4a8e351
RaenonX JELLYCAT <maplestory0710@gmail.com>
2017-07-10 16:11:36 +0800

    Add tips

--- error.py
+++ error.py
@@ -3,6 +3,7 @@
 import time
 
 class error(object):
+    user_manual_url = 'https://sites.google.com/view/jellybot'
 
     class webpage(object):
 
@@ -18,11 +19,11 @@ class error(object):
         
         @staticmethod
         def invalid_thing(name_of_thing, thing):
-            return u'不合法的{}: {}。請查看使用說明。'.format(name_of_thing, thing)
+            return u'不合法的{}: {}。請查看使用說明書( {} )。'.format(name_of_thing, thing, error.user_manual_url)
 
         @staticmethod
         def lack_of_thing(name_of_thing):
-            return u'缺少{nm}。請修正您所提供的{nm}成正確的格式。'.format(nm=name_of_thing)
+            return u'缺少{nm}。請修正您所提供的{nm}成正確的格式。詳細說明請參閱使用說明書( {um} )'.format(nm=name_of_thing, um=error.user_manual_url)
 
         @staticmethod
         def no_result():
@@ -31,22 +32,23 @@ class error(object):
         @staticmethod
         def restricted(permission=None):
             return u'已限制的功能。{}'.format(
-                u'\n\n需求權限: {}+'.format(permission) if permission is not None else u'')
+                u'\n\n需求權限: {}+\n\n權限相關說明請參閱使用說明書( {} )'.format(permission) if permission is not None else u'', error.user_manual_url)
 
         @staticmethod
         def incorrect_channel(available_in_1v1=True, available_in_room=False, available_in_group=False):
-            return u'無法於此類型的頻道使用。請至下列頻道:\n{} {} {}'.format(
+            return u'無法於此類型的頻道使用。請至下列頻道:\n{} {} {}\詳細使用說明請參閱使用說明書( {} )'.format(
                 u'[ 私訊 ]' if available_in_1v1 else u'[ - ]',
                 u'[ 群組 ]' if available_in_group else u'[ - ]',
-                u'[ 房間 ]' if available_in_room else u'[ - ]')
+                u'[ 房間 ]' if available_in_room else u'[ - ]',
+                error.user_manual_url)
 
         @staticmethod
         def incorrect_param(param_name, correct):
-            return u'無法辨認。如果要使用這個功能，{}必須為{}。'.format(param_name, correct)
+            return u'無法辨認。如果要使用這個功能，{}必須為{}。詳細使用方是請參閱使用說明書( {} )'.format(param_name, correct, error.user_manual_url)
 
         @staticmethod
         def unable_to_determine():
-            return u'無法判斷指令，請檢閱使用說明書。'
+            return u'無法判斷指令，請檢閱使用說明書( {} )。'.format(error.user_manual_url)
 
         @staticmethod
         def pair_not_exist():
@@ -55,4 +57,5 @@ class error(object):
     class message(object):
         @staticmethod
         def insufficient_space_for_command():
-            return u'偵測到1個空格。如果是要使用指令的話，指令和參數之間需要2個空格。如果只是作為參數的話，請無視此提示訊息。'
+            return u'偵測到1個空格。\n如果是使用指令的話，指令和參數之間需要2個空格。詳細使用方是請參閱使用說明書( {} )。\n如果只是作為參數的話，請無視此提示訊息。'.format(
+                error.user_manual_url)
