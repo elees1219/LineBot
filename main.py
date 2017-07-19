9b31744713eb57dde78c2897145f36eed3eb9f3e
RaenonX JELLYCAT <maplestory0710@gmail.com>
2017-07-20 01:29:17 +0800

    optimize main code 'A'

--- main.py
+++ main.py
@@ -63,3 +63,87 @@ class command(object):
             text = error.main.restricted(3)
 
         return text
+
+    def A(self, src, params_array, pinned=False):
+        """'A'DD Keyword Pair"""
+        if params[4] is not None:
+            action_kw = params[1]
+            kw = params[2]
+            action_rep = params[3]
+            rep = params[4]
+             
+            if action_kw != 'STK':
+                results = None
+                text = error.main.incorrect_param(u'參數1', u'STK')
+            elif not string_is_int(kw):
+                results = None
+                text = error.main.incorrect_param(u'參數2', u'整數數字')
+            elif action_rep != 'PIC':
+                results = None
+                text =  error.main.incorrect_param(u'參數3', u'PIC')
+            else:
+                if string_is_int(rep):
+                    rep = sticker_png_url(rep)
+                    url_val_result = True
+                else:
+                    url_val_result = url_val_result = True if validators.url(rep) and urlparse(rep).scheme == 'https' else False
+
+                if type(url_val_result) is bool and url_val_result:
+                    results = kwd.insert_keyword(kw, rep, new_uid, pinned, True, True)
+                else:
+                    results = None
+                    text = error.main.incorrect_param(u'參數4', u'HTTPS協定，並且是合法的網址。')
+        elif params[3] is not None:
+            rep = params[3]
+
+            if params[2] == 'PIC':
+                kw = params[1]
+
+                if string_is_int(rep):
+                    rep = sticker_png_url(rep)
+                    url_val_result = True
+                else:
+                    url_val_result = True if validators.url(rep) and urlparse(rep).scheme == 'https' else False
+
+                if type(url_val_result) is bool and url_val_result:
+                    results = kwd.insert_keyword(kw, rep, new_uid, pinned, False, True)
+                else:
+                    results = None
+                    text = error.main.incorrect_param(u'參數3', u'HTTPS協定，並且是合法的網址。')
+            elif params[1] == 'STK':
+                kw = params[2]
+
+                if string_is_int(kw):
+                    results = kwd.insert_keyword(kw, rep, new_uid, pinned, True, False)
+                else:
+                    results = None
+                    text = error.main.incorrect_param(u'參數2', u'整數數字')
+            else:
+                text = error.main.unable_to_determine()
+                results = None
+        elif params[2] is not None:
+            kw = params[1]
+            rep = params[2]
+
+            results = kwd.insert_keyword(kw, rep, new_uid, pinned, False, False)
+        else:
+            results = None
+            text = error.main.lack_of_thing(u'參數')
+
+        if results is not None:
+            text = u'已新增回覆組。{}\n'.format(u'(置頂)' if pinned else '')
+            for result in results:
+                text += kw_dict_mgr.entry_basic_info(result)
+
+        return text
+
+    def M(self, src, params_array, pinned):
+        """'M'AKE pinned Keyword Pair"""
+        if pinned and permission_level(params.pop(1)) < 1:
+            text = error.main.restricted(1)
+        elif not is_valid_user_id(new_uid):
+            text = error.main.unable_to_receive_user_id()
+        else:
+            text = self.A(src, params_array)
+
+            return text
