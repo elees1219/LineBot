4a13ab14f060080164696ea1fa15cf7d18597793
RaenonX JELLYCAT <maplestory0710@gmail.com>
2017-07-19 01:55:28 -0500

    debug

--- db/msg_track.py
+++ db/msg_track.py
@@ -188,7 +188,7 @@ class message_tracker(object):
         if count <= 0:
             ret['limited'] = error.main.no_result()
         else:
-            ret['limited'] = u'\n\n'.join([message_tracker.entry_detail(data, group_ban) for data in data_list[0:limit - 1]])
+            ret['limited'] = u'\n\n'.join([message_tracker.entry_detail(data, group_ban) for data in data_list[0:limit]])
             if count - limit > 0:
                 ret['limited'] += u'\n\n...還有{}筆資料'.format(count - limit)
 
