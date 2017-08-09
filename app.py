c2285d4321a8113667555fac5ddaa7761328a73a
RaenonX JELLYCAT <maplestory0710@gmail.com>
2017-08-10 03:06:02 +0800

    debug

--- app.py
+++ app.py
@@ -273,25 +273,25 @@ def get_error_list():
 def get_error_message(timestamp):
     sys_data.view_webpage()
     content = webpage_generator.get_content(webpage_auto_gen.content_type.Error, timestamp)
-    return webpage_auto_gen.webpage.html_render(content)
+    return webpage_auto_gen.webpage.html_render(content, u'錯誤訊息')
 
 @app.route("/query/<timestamp>", methods=['GET'])
 def full_query(timestamp):
     sys_data.view_webpage()
     content = webpage_generator.get_content(webpage_auto_gen.content_type.Query, timestamp)
-    return webpage_auto_gen.webpage.html_render(content)
+    return webpage_auto_gen.webpage.html_render(content, u'查詢結果')
 
 @app.route("/info/<timestamp>", methods=['GET'])
 def full_info(timestamp):
     sys_data.view_webpage()
     content = webpage_generator.get_content(webpage_auto_gen.content_type.Info, timestamp)
-    return webpage_auto_gen.webpage.html_render(content)
+    return webpage_auto_gen.webpage.html_render(content, u'詳細資料')
 
 @app.route("/full/<timestamp>", methods=['GET'])
 def full_content(timestamp):
     sys_data.view_webpage()
     content = webpage_generator.get_content(webpage_auto_gen.content_type.Text, timestamp)
-    return webpage_auto_gen.webpage.html_render(content)
+    return webpage_auto_gen.webpage.html_render(content, u'完整資訊')
 
 @app.route("/ranking/<type>", methods=['GET'])
 def full_ranking(type):
@@ -303,7 +303,7 @@ def full_ranking(type):
     else:
         content = error.webpage.no_content()
         
-    return webpage_auto_gen.webpage.html_render(content)
+    return webpage_auto_gen.webpage.html_render(content, u'完整排名')
 
 
 @handler.add(MessageEvent, message=TextMessage)
