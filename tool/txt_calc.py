bd21744e0afb329c093a5a846cd789b00396b677
RaenonX JELLYCAT <maplestory0710@gmail.com>
2017-08-28 16:20:59 +0800

    debug - wrong HEX determination

--- tool/txt_calc.py
+++ tool/txt_calc.py
@@ -3,6 +3,8 @@
 class text_calculator(object):
     @staticmethod
     def calc(text):
+        if text.startswith('0'):
+            return
         try:
             result = ''
             if 'result=' not in text:
